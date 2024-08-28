8f589cc3de4f50d449244e8497a8a306e2810e92
Andres Torres <andresalbertotorresgarcia@gmail.com>
2024-08-27 19:27:59 -0500

    feat(sales-order): added sales order item entity

---
+++ src/sales/domain/model/sales-order-item.js
@@ -0,0 +1,22 @@
+export class SalesOrderItem {
+    constructor({orderId, productId, quantity, unitPrice}) {
+        this._orderId = orderId; // se inicia con _ para poder tener en cuenta que una variable privada
+        this._productId = productId;
+        this._quantity = quantity;
+        this._unitPrice = unitPrice;
+    }
+
+    CalculateItemPrice(){
+        return this._quantity * this._unitPrice;
+    }
+
+    get orderId() {
+        return this._orderId;
+    }
+
+    get productId() {
+        return this._productId;
+    }
+}
+
+
